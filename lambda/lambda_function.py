26523c79f4e423f321a91ef7b885868ce15730f5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-28 19:08:12 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -63,18 +63,18 @@ class CriarRoletaIntentHandler(AbstractRequestHandler):
         
         for i in range (len(items)):
             if ( i == len(items)-1):
-                str += ' e ' + item
+                str += ' e ' + items[i]
             else:
-                str += item + ', '
+                str += items[i] + ', '
         
                
-        rouleteAttributes = {
-            "name" : rouletteName,
-            "items": items 
-        }
+        #rouleteAttributes = {
+        #    "name" : rouletteName,
+        #    "items": items 
+        #}
 
-        attributes_manager.persistent_attributes = rouleteAttributes
-        attributes_manager.save_persistent_attributes()
+        #attributes_manager.persistent_attributes = rouleteAttributes
+        #attributes_manager.save_persistent_attributes()
         
         return (
             handler_input.response_builder
