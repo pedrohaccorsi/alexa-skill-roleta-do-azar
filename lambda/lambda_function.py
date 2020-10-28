30bfad01714cd07a1b4c5bc4513663efabbd69ef
Pedro Accorsi <pedrohaccorsi@gmail.com>
2020-10-28 01:13:43 -0300

    teste

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
-# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-# session persistence, api calls, and more.
-# This sample is built using the handler classes approach in skill builder.
 import logging
 import ask_sdk_core.utils as ask_utils
 
